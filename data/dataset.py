fixed data scaling in load()

data/dataset.py
@@ -21,14 +21,19 @@ class Dataset(torch.utils.data.Dataset):
         self._mat = self._mat.astype(self._precision)
 
         # normalize the dataset between values of o to 1
+        self._scaler = None
         if normalize:
-            _scaler = MinMaxScaler(feature_range=bounds)
-            _scaler.fit(self._mat)
-            self._mat = _scaler.transform(self._mat)
+            self._scaler = MinMaxScaler(feature_range=bounds)
+            self._scaler.fit(self._mat)
+            self._mat = self. _scaler.transform(self._mat)
 
     @property
     def sample_size(self) -> int:
         return 1
+    
+    def scale_back(self, data):
+        data = np.array(data, dtype=self._precision)
+        return self._scaler.inverse_transform(data)
 
     def __len__(self):
         return max(1, len(self._mat) - self._f_seq - self._seq)

main.py
@@ -130,8 +130,8 @@ def load():
         if index == future_steps:
             index = 0
 
-    pred_data = np.array([[i, d] for i, d in enumerate(pred_data)])
-    actual_data = np.array([[i, d] for i, d in enumerate(actual_data)])
+    pred_data = np.array([[i, dataset.scale_back([[d]])] for i, d in enumerate(pred_data)])
+    actual_data = np.array([[i, dataset.scale_back([[d]])] for i, d in enumerate(actual_data)])
     d_type = config_dict["dataset_args"]["d_type"]
     plot_curve(data=[actual_data, pred_data], data_name=["actual", "prediction"],
                title=f"Look ahead: {future_steps}", save_path=f"{root_folder}/look_ahead_{future_steps}_{d_type}.png")