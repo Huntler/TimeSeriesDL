c97ae60120d8e0b44d5e672eacb6f9148eda6abd
Huntler <jonas.leuchtenberger@rwth-aachen.de>
2024-05-21 20:18:46 +0200

    clean dataset loader

--- TimeSeriesDL/data/dataset.py
+++ TimeSeriesDL/data/dataset.py
@@ -1,4 +1,5 @@
 
+"""This module contains the Dataset loader."""
 from typing import List, Tuple
 from sklearn.preprocessing import MinMaxScaler
 import scipy.io
@@ -7,8 +8,14 @@ import numpy as np
 
 
 class Dataset(torch.utils.data.Dataset):
+    """The dataset class loads a dataset from a scipy-matrix and normalize all values according 
+    to boundaries defined. On very large dataset, this class can run in system out of memory issues.
+
+    Args:
+        torch (torch.utils.data.Dataset): Based on torch's dataset class.
+    """
     def __init__(self, d_type: str = "train", normalize: bool = True, bounds: Tuple[int] = (0, 1),
-                future_steps: int= 1, sequence_length: int = 1, precision: np.dtype = np.float32):
+                 future_steps: int = 1, sequence_length: int = 1, precision: np.dtype = np.float32):
         super(Dataset, self).__init__()
 
         self._precision = precision
@@ -16,9 +23,9 @@ class Dataset(torch.utils.data.Dataset):
         self._f_seq = future_steps
 
         # load the dataset specified
-        self._file = f"./data/{d_type}.mat"
-        self._mat = scipy.io.loadmat(self._file).get(f"X{d_type}")
-        self._mat = self._mat.astype(self._precision)
+        self._d_type = d_type
+        self._file = f"./data/{self._d_type}.mat"
+        self._mat = self.load_data()
 
         # normalize the dataset between values of o to 1
         self._scaler = None
@@ -27,11 +34,33 @@ class Dataset(torch.utils.data.Dataset):
             self._scaler.fit(self._mat)
             self._mat = self. _scaler.transform(self._mat)
 
+    def load_data(self) -> np.array:
+        """Loads the dataset from the path self._file which is generated as './data/{d_type}.mat'.
+
+        Returns:
+            np.array: The dataset.
+        """
+        mat = scipy.io.loadmat(self._file).get(f"X{self._d_type}")
+        return mat.astype(self._precision)
+
     @property
     def sample_size(self) -> int:
+        """Returns the sample size of the dataset.
+
+        Returns:
+            int: The dataset's sample size.
+        """
         return 1
-    
-    def scale_back(self, data):
+
+    def scale_back(self, data: List):
+        """Scales the given data back using the inverse scaler.
+
+        Args:
+            data (List): Data.
+
+        Returns:
+            np.array: Data scaled to input range.
+        """
         data = np.array(data, dtype=self._precision)
         return self._scaler.inverse_transform(data)
 
@@ -39,37 +68,6 @@ class Dataset(torch.utils.data.Dataset):
         return max(1, len(self._mat) - self._f_seq - self._seq)
 
     def __getitem__(self, index):
-        #X = np.zeros((self._seq, 1), dtype=self._precision)
-        #y = np.zeros((self._f_seq, 1), dtype=self._precision)
-        X = self._mat[index:self._seq + index]
+        x = self._mat[index:self._seq + index]
         y = self._mat[self._seq + index:self._seq + index + self._f_seq]
-        return X, y
-
-        if index == 0:
-            return X, y
-        
-        if index > len(self):
-            return None, None
-
-        # calculate indecies for adding to the sequence
-        y_start = index - self._f_seq
-        y_end = index
-        x_start = y_start - self._seq
-        x_end = y_start
-
-        # define the label sequence y
-        # occurs also if x_end < 0
-        if y_start <= 0:
-            y[-self._f_seq - index:] = self._mat[index:self._f_seq + index]
-            return X, y
-
-        y = self._mat[index - self._f_seq:index]
-        y = y.astype(self._precision)
-
-        # define the training data X
-        if x_start < 0:
-            X[self._seq - x_end:] = self._mat[:x_end]
-            return X, y
-
-        X = self._mat[x_start:x_end]
-        return X, y
+        return x, y
