401ed617e85fb28a6818dd603bf88e8aa67200f4
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2024-05-25 18:35:13 +0200

    add encode_dataset to package

---
+++ TimeSeriesDL/data/encode.py
@@ -0,0 +1,49 @@
+"""Encodes a dataset and stores it."""
+from typing import Dict
+from tqdm import tqdm
+from scipy.io import savemat
+import torch
+from TimeSeriesDL.data import Dataset
+from TimeSeriesDL.model.conv_ae_model import ConvAE
+
+
+def encode_dataset(
+    train_args: Dict, export_path: str = "./examples/train_encoded.mat"
+) -> None:
+    """Loads a trained ConvAE using the config dictionary.
+
+    Args:
+        train_args (Dict): The config of a trained ConvAE.
+    """
+    # check if the model to load is trained and type AE
+    if train_args["model_name"] != "ConvAE":
+        print("To encode a dataset, an AE model is required.")
+        exit(1)
+
+    if not train_args["model_path"]:
+        print("The AE model needs to be trained first.")
+        exit(1)
+
+    # load the AE and prevent logging
+    train_args["model"]["log"] = False
+    ae = ConvAE(**train_args["model"])
+    ae.load(path=train_args["model_path"])
+
+    # load the dataset which should be encoded, make sure to disable AE mode
+    train_args["dataset"]["ae_mode"] = False
+    data = Dataset(**train_args["dataset"])
+
+    encoded = []
+    for i in tqdm(range(0, data.sample_size, train_args["dataset"]["sequence_length"])):
+        # get the data as tensor
+        x, _ = data[i]
+        x = torch.unsqueeze(torch.tensor(x), 0)
+
+        # encode the data and unwrap the batch
+        x = ae.encode(x)
+        x = torch.transpose(x, 2, 1)
+        x = list(x.cpu().detach().numpy()[0, :, :])
+        encoded += x
+
+    # save the encoded dataset
+    savemat(export_path, {"train": encoded})
--- examples/data_generation/encode.py
+++ examples/data_generation/encode.py
@@ -1,45 +1,16 @@
 """Example usage of the any model."""
+
 import argparse
-import torch
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from scipy.io import savemat
-from TimeSeriesDL.data import Dataset
-from TimeSeriesDL.model.conv_ae_model import ConvAE
+from TimeSeriesDL.data import encode_dataset
 from TimeSeriesDL.utils import config
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", help="Configuration file to load", required=True)
-args = parser.parse_args()
 
-train_args = config.get_args(args.config)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", help="Configuration file to load", required=True
+    )
+    args = parser.parse_args()
 
-# check if the model to load is trained and type AE
-if train_args["model_name"] != "AE":
-    print("To encode a dataset, an AE model is required.")
-    exit(1)
-
-if not train_args["model_path"]:
-    print("The AE model needs to be trained first.")
-    exit(1)
-
-# load the AE and prevent logging
-train_args["model"]["log"] = False
-ae = ConvAE(**train_args["model"])
-ae.load(path=train_args["model_path"])
-
-# load the dataset which should be encoded, make sure to disable AE mode
-train_args["dataset"]["ae_mode"] = False
-data = Dataset(**train_args["dataset"])
-dataloader = DataLoader(data)
-
-encoded = []
-for x, _ in tqdm(dataloader):
-    # encode the data and unwrap the batch
-    x = ae.encode(x)
-    x = torch.transpose(x, 2, 1)
-    x = list(x.cpu().detach().numpy()[0, :, :])
-    encoded.append(x)
-
-# save the encoded dataset
-savemat("./examples/train_encoded.mat", {"train": encoded})
+    train_args = config.get_args(args.config)
+    encode_dataset(train_args)
