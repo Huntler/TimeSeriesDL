fix visualize dataset scaling

TimeSeriesDL/debug/visualize_dataset.py
@@ -41,7 +41,8 @@ class VisualizeDataset:
     """This class visualizes a dataset, with the ability to overwrite label names, select features
     to visualizes, and possibility to overlay a second dataset.
     """
-    def __init__(self, dataset: Dataset, name: str = None, overlay_mode: bool = False) -> None:
+    def __init__(self, dataset: Dataset, name: str = None, overlay_mode: bool = False,
+                 scale_back: bool = False) -> None:
         """Initializes a visualization object to show insights into a dataset.
 
         Args:
@@ -50,8 +51,10 @@ class VisualizeDataset:
             name (str, optional): The name of the dataset. Defaults to the dataset file name.
             overlay_mode (bool, optional): This object should be used as an overlay.
             Defaults to False.
+            scale_back (bool): Scales the dataset back if it was normalized. Defaults to False.
         """
         self._dataset = dataset
+        self._scale_back = scale_back
         self._name = name if name else self._dataset.d_type
         assert len(self._dataset.shape) == 3, "Expected dataset to have two dimensions."
 
@@ -136,8 +139,8 @@ class VisualizeDataset:
             return num_features, 1
 
         h, w = max_h, math.ceil(num_features / max_h)
-        if (h * w - num_features) % w == 0:
-            return h - (h * w - num_features) // 2, w
+        if h * w - num_features > w:
+            return h - ((h * w - num_features) % w), w
         return h, w
 
     def visualize(self, start: int = 0, end: int = -1, size: int = 3, save: str = None) -> None:
@@ -161,9 +164,15 @@ class VisualizeDataset:
         _overlay_data = None
         if self._overlay:
             _overlay_data = self._overlay.dataset.slice(start, end, self._feature)
+            if self._scale_back:
+                _overlay_data = self._overlay.dataset.scale_back(_overlay_data[:, 0, :])
+                _overlay_data = np.expand_dims(_overlay_data, 1)
 
         # slice the dataset as required to view start/end/selected features
         data = self._dataset.slice(start, end, self._feature)
+        if self._scale_back:
+            data = self._dataset.scale_back(data[:, 0, :])
+            data = np.expand_dims(data, 1)
 
         # setup graph and layout
         grid = self._get_grid(4, len(self._feature))

examples/data_generation/generate.py
@@ -20,6 +20,7 @@ if not args.samples:
 path = args.path if args.path else"examples/train_1.mat"
 samples = int(args.samples)
 version = int(args.version) if args.version else 0
+print(f"Generating version {version}")
 
 d = {}
 if version == 0:
@@ -46,9 +47,9 @@ elif version == 1:
     savemat(path, d)
 
 elif version == 2:
-    # same as version 1 but shifted by x=2
+    # same as version 1 but shifted by x=5
     # Generate a sequence of x values
-    x = np.linspace(2, 12, samples)
+    x = np.linspace(1, 1, samples)
 
     # Calculate y values for each function
     y1 = (x**2 + 2*x + 1) * np.exp(-x*0.5)
@@ -61,16 +62,17 @@ elif version == 2:
     savemat(path, d)
 
 elif version == 3:
-    # same as version 1 but shifted by x=2 and y*0.9
+    # same as version 1 but shifted by x=1 and y*0.2
     # Generate a sequence of x values
-    x = np.linspace(1, 11, samples)
+    x = np.linspace(0, 10, samples)
 
     # Calculate y values for each function
-    y1 = (x**2 + 2*x + 1) * np.exp(-x*0.5) * 0.9
-    y2 = np.exp(y1) * 0.9
-    y3 = np.sin(y1*2) * 0.9
-    y4 = np.sin(np.exp(x/2)) * 0.9
-    y5 = np.log(x + 1) * 0.9
+    factor = 0.8
+    y1 = (x**2 + 2*x + 1) * np.exp(-x*0.5) * factor
+    y2 = np.exp(y1) * factor
+    y3 = np.sin(y1*2) * factor
+    y4 = np.sin(np.exp(x/2)) * factor
+    y5 = np.log(x + 1) * factor
 
     d = {"train_1": y1, "train_2": y2, "train_3": y3, "train_4": y4, "train_5": y5}
     savemat(path, d)

examples/visualize/show_dataset.py
@@ -3,8 +3,8 @@ import argparse
 import os
 from typing import Dict
 
-import numpy as np
 import torch
+import numpy as np
 from scipy.io import savemat
 from tqdm import trange
 from TimeSeriesDL.data import Dataset
@@ -19,10 +19,10 @@ def predicted_dataset(train_args: Dict, dataset: Dataset) -> Dataset:
 
     Args:
         train_args (Dict): The config args of a trained model.
-        dataset (Dataset): The dataset
+        dataset (Dataset): The dataset which serves as an input.
 
     Returns:
-        Dataset: _description_
+        Dataset: The predicted dataset.
     """
     train_args["model"]["log"] = False
     model: BaseModel = config.get_model(train_args["model_name"])(**train_args["model"])
@@ -35,7 +35,7 @@ def predicted_dataset(train_args: Dict, dataset: Dataset) -> Dataset:
     full_sequence[0:window_len, :] = dataset.slice(0, window_len)
 
     # predict based on sliding window
-    print("Predicting...")
+    print("Predicting...", train_args["dataset"]["custom_path"])
     for i in trange(0, dataset.sample_size - window_len, f_len):
         window = full_sequence[i:i + window_len]
         window = torch.tensor(window, device=train_args["device"], dtype=torch.float32)
@@ -65,29 +65,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--config", type=str, required=True)
-    parser.add_argument("--compare", default=False, action="store_true")
+    parser.add_argument("--compare-to", type=str, default=None)
     parser.add_argument("--features", type=int, nargs="+")
+    parser.add_argument("--unscale", action="store_true", default=False)
     parser.add_argument("--start", type=int, default=0)
     parser.add_argument("--end", type=int, default=-1)
-    parser.add_argument("--output", type=str)
+    parser.add_argument("--output", type=str, default=None)
 
     # define args
     args = parser.parse_args()
     train_args = config.get_args(args.config)
+    if args.compare_to:
+        train_args["dataset"]["custom_path"] = args.compare_to
 
     # load the dataset and put it into the visualizer
     data = Dataset(**train_args["dataset"])
-    vis = VisualizeDataset(data, name="Train")
+    vis = VisualizeDataset(data, name="Input", scale_back=args.unscale)
 
     # load the second dataset to compare to the already loaded one if the argument is provided
-    if args.compare:
+    if args.compare_to:
         data2 = predicted_dataset(train_args, data)
         overlay = VisualizeDataset(data2, name="Predicted", overlay_mode=True)
         vis.set_overlay(overlay)
 
     # visualize the dataset(s)
-    features = args.get("features", list(range(len(data.label_names))))
+    features = args.features if args.features else list(range(len(data.label_names)))
     vis.set_feature(features)
 
     # test save last configuration but of all samples
-    vis.visualize(start=args.start, end=args.end, save=args.get("output", None))
+    vis.visualize(start=args.start, end=args.end, save=args.output)