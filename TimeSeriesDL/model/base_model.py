59946272cf894b2be9d4a0bcaee6c7443d41e796
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2024-06-06 10:57:01 +0200

    modify BaseModel to work with Trainer

--- TimeSeriesDL/model/base_model.py
+++ TimeSeriesDL/model/base_model.py
@@ -1,18 +1,10 @@
 """This module contains the base model."""
 
-from datetime import datetime
 from typing import List
 import os
 
-import numpy as np
 import torch
 from torch import nn
-from torch.optim.lr_scheduler import _LRScheduler
-from torch.optim.optimizer import Optimizer
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
-from TimeSeriesDL.loss import LossMeasurementSuite
 
 
 class BaseModel(nn.Module):
@@ -22,19 +14,16 @@ class BaseModel(nn.Module):
         nn (nn.Module): Torch nn module.
     """
 
-    def __init__(self, name: str, save_every: int = 0, tag: str = "", log: bool = True) -> None:
+    def __init__(self, name: str, tag: str = "") -> None:
         """Initializes the TensorBoard logger and checks for available GPU(s).
 
         Args:
             writer (SummaryWriter, optional): TensorBoard writer. Defaults to a generic path.
         """
-        super(BaseModel, self).__init__()
+        super().__init__()
 
         # enable tensorboard
-        self._writer = False
-        self._init_writer(name, tag, log)
-        self.__sample_position = 0
-        self._save_every = save_every
+        self._init_log_path(name, tag)
 
         # check for gpu
         self._device = "cpu"
@@ -44,37 +33,23 @@ class BaseModel(nn.Module):
 
         self._precision = torch.float32
 
-        # define object which where defined by children of this class
-        self._loss_suite = LossMeasurementSuite(self._writer)
-        self._scheduler: _LRScheduler = None
-        self._optim: Optimizer = None
-
-        self.test_stats = None
-
-    def _init_writer(self, name: str, tag: str, log: bool) -> None:
-        """Initializes the TensorBoard writer.
+    def _init_log_path(self, name: str, tag: str) -> None:
+        """Initializes the Log path writer.
 
         Args:
             name (str): The name of the model folder.
             tag (str): The tag name of a primary folder.
-            log (bool): Enable logging.
         """
-        if not log:
-            return
+        path = f"runs/{tag}/{name}"
 
-        now = datetime.now()
-        self._tb_sub = now.strftime("%d%m%Y_%H%M%S")
-        path = f"runs/{tag}/{name}/{self._tb_sub}"
-
-        # check if TensorBoard path exists, if so add "_<increment>" to the path string
+        # check if log path exists, if so add "_<increment>" to the path string
         for i in range(100):
             if os.path.exists(path):
                 path += f"_{i}"
             else:
                 break
 
-        self._tb_path = path
-        self._writer = SummaryWriter(self._tb_path)
+        self._tb_path = path + "/"
 
     @property
     def log_path(self) -> str:
@@ -132,7 +107,8 @@ class BaseModel(nn.Module):
         params = self.state_dict()
         post_fix = f"_{post_fix}" if post_fix is not None else ""
         if not os.path.exists(f"{self._tb_path}/models/"):
-            os.mkdir(f"{self._tb_path}/models/")
+            os.makedirs(f"{self._tb_path}/models/")
+
         torch.save(params, f"{self._tb_path}/models/model{post_fix}.torch")
 
     def load(self, path: str) -> None:
@@ -161,126 +137,6 @@ class BaseModel(nn.Module):
                                  for this.
         """
         raise NotImplementedError
-
-    def learn(self, train, validate=None, test=None, epochs: int = 1):
-        """Trains the model on a dataset. Valdiation- and Testdatasets can be set as well.
-
-        Args:
-            train (DataLoader): Dataset to train on.
-            validate (Dataset, optional): Validate model on this dataset. Defaults to None.
-            test (Dataset, optional): Test model on this dataset. Defaults to None.
-            epochs (int, optional): Run training for given amount of epochs. Defaults to 1.
-        """
-        # set the model into training mode
-        self.train()
-
-        # check if the dataset is wrapped within a Dataloader
-        assert isinstance(
-            train, DataLoader
-        ), "Please provide the dataset wrapped in a torch DataLoader."
-
-        # run for n epochs specified
-        pbar = tqdm(
-            total=epochs * len(train) * train.batch_size,
-            desc=f"Epochs {epochs}",
-            leave=True,
-        )
-        for e in range(epochs):
-            # run for each batch in training set
-            for x, y_hat in train:
-                x = x.to(self._device)
-                y_hat = y_hat.to(self._device)
-
-                # perform the presiction and measure the loss between the prediction
-                # and the expected output
-                y = self(x)
-
-                # calculate the gradient using backpropagation of the loss
-                loss, losses = self._loss_suite.calulate(
-                    y, y_hat, self.__sample_position
-                )
-
-                # reset the gradient and run backpropagation
-                self._optim.zero_grad()
-                loss.backward()
-                self._optim.step()
-
-                # log for the statistics
-                self._loss_suite.add_epoch_data(losses)
-
-                self.__sample_position += x.size(0)
-                pbar.update(train.batch_size)
-
-            # if there is an adaptive learning rate (scheduler) available
-            if self._scheduler:
-                self._scheduler.step()
-                lr = self._scheduler.get_last_lr()[0]
-                self._writer.add_scalar("Epoch/Train/learning_rate", lr, e)
-
-            # log for the statistics
-            self._loss_suite.log_epoch(e)
-
-            # runn a validation of the current model state
-            if validate:
-                # set the model to eval mode, run validation and set to train mode again
-                self.eval()
-                _ = self.validate(validate, e)
-                self.train()
-
-            if test:
-                self.eval()
-                _ = self.validate(test, e, "Test")
-                self.train()
-
-            # save the model after every n epochs
-            if self._save_every > 0 and (e + 1) % self._save_every == 0:
-                self.save_to_default(e + 1)
-
-        self.eval()
-        sample = np.expand_dims(train.dataset[0][0], axis=0)
-        self._writer.add_graph(
-            self, torch.tensor(sample, dtype=self._precision, device=self._device)
-        )
-        self._writer.flush()
-
-    def validate(self, loader: DataLoader, epoch: int, tag: str = "Validate") -> float:
-        """This method validates/tests the model on a different dataset and logs
-        losses/accuracies to the tensorboard.
-
-        Args:
-            loader (DataLoader): The dataset to validate/test on.
-            epoch (int): The epoch in which the model is validated/tested.
-            tag (str, optional): The tag describing which values are logged.
-            Defaults to "Validate".
-
-        Returns:
-            float: The models accuracy on the given dataset.
-        """
-        accuracies = []
-        self._loss_suite.set_tag(tag)
-
-        # predict all y's of the validation set and append the model's accuracy
-        # to the list
-        for x, y_hat in loader:
-            y = self.predict(x, as_array=False)
-            y_hat = y_hat.to(self._device)
-
-            loss, data = self._loss_suite.calulate(y, y_hat)
-            self._loss_suite.add_epoch_data(data)
-
-            accuracies.append(1 - loss.item())
-
-        # calculate some statistics based on the data collected
-        accuracy = np.mean(np.array(accuracies))
-        variance = np.mean(np.var(np.array(accuracies)))
-
-        self._loss_suite.log_epoch(epoch)
-
-        # log to the tensorboard if wanted
-        self._writer.add_scalar(f"{tag}/accuracy_mean", accuracy, epoch)
-        self._writer.add_scalar(f"{tag}/accuracy_var", variance, epoch)
-
-        return accuracy
 
     def predict(self, x: torch.tensor, as_array: bool = False) -> List:
         """This method only predicts future steps based on the given curve described by
